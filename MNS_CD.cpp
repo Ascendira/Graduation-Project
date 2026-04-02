4020da028b7cae770767738e9974452b553f290c
Ascendira <liangjincheng_7@163.com>
2026-04-02 20:33:55 +0800

    verify the accuracy of binary grouping

--- MNS_CD.cpp
+++ MNS_CD.cpp
@@ -863,7 +863,7 @@ static void getGroupFlux(UserDataType *data, N_Vector y, realtype *J_M)
       realtype M1_g = yd[p_base + 2 * g + 1];
 
       // 1. 评估界面处的吸收速率 (wpEff) 和发射速率修正 (emission_ratio)
-      realtype n_b = GMap[g - 1].n_max;
+      realtype n_b = GMap[g - 1].n_center;
       realtype r_b =
           pow(3.0 * IMaterial->cVol[pref] * n_b / (4.0 * pi), 1.0 / 3.0);
 
@@ -1004,12 +1004,12 @@ static int f(realtype t, N_Vector y, N_Vector ydot, void *user_data)
 
     for (int g = 1; g < numGroups; g++)
     {
-      ydotd[p_base + 2 * g + 1] = data->J_M[fluxIndex(p, g)] * GMap[g].n_min - data->J_M[fluxIndex(p, g + 1)] * GMap[g].n_max;
-      ydotd[p_base + 2 * g] = ydotd[p_base + 2 * g + 1] / GMap[g].n_center;
+      ydotd[p_base + 2 * g] = data->J_M[fluxIndex(p, g)] - data->J_M[fluxIndex(p, g + 1)];
+      ydotd[p_base + 2 * g + 1] = ydotd[p_base + 2 * g] * GMap[g].n_center;
 
       for (int c = 0; c < numComp; c++)
       {
-        total_sol_cons[c] += IMaterial->X[pref][c] * ydotd[p_base + 2 * g + 1];
+        total_sol_cons[c] += IMaterial->X[pref][c] * ydotd[p_base + 2 * g + 1] * GMap[g].width;
       }
     }
   }
